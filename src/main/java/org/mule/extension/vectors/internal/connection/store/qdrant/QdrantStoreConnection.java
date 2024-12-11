01a86e85428ed8d390f9b2184602128bc56b99e4
Tommaso Bolis <tbolis@mulesoft.com>
2024-12-11 13:20:34 +0100

    Implements connection provider for qdrant

--- src/main/java/org/mule/extension/vectors/internal/connection/store/qdrant/QdrantStoreConnection.java
+++ src/main/java/org/mule/extension/vectors/internal/connection/store/qdrant/QdrantStoreConnection.java
@@ -1,7 +1,13 @@
 package org.mule.extension.vectors.internal.connection.store.qdrant;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.QdrantGrpcClient;
+import io.qdrant.client.grpc.QdrantOuterClass;
 import org.mule.extension.vectors.internal.connection.store.BaseStoreConnection;
+import org.mule.extension.vectors.internal.connection.store.elasticsearch.ElasticsearchStoreConnection;
 import org.mule.extension.vectors.internal.constant.Constants;
+import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.meta.ExpressionSupport;
 import org.mule.runtime.extension.api.annotation.Expression;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
@@ -9,14 +15,22 @@ import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.annotation.param.display.Example;
 import org.mule.runtime.extension.api.annotation.param.display.Password;
 import org.mule.runtime.extension.api.annotation.param.display.Placement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class QdrantStoreConnection implements BaseStoreConnection {
 
-  private String host;
-  private int gprcPort;
-  private boolean useTLS;
-  private String textSegmentKey;
-  private String apiKey;
+  private static final Logger LOGGER = LoggerFactory.getLogger(QdrantStoreConnection.class);
+
+  private final String host;
+  private final int gprcPort;
+  private final boolean useTLS;
+  private final String textSegmentKey;
+  private final String apiKey;
+
+  private QdrantClient client;
 
   public QdrantStoreConnection(String host, int gprcPort, boolean useTLS, String textSegmentKey, String apiKey) {
     this.host = host;
@@ -46,23 +60,65 @@ public class QdrantStoreConnection implements BaseStoreConnection {
     return apiKey;
   }
 
+  public QdrantClient getClient() {
+    return client;
+  }
+
   @Override
   public String getVectorStore() {
     return Constants.VECTOR_STORE_QDRANT;
   }
 
   @Override
-  public void connect() {
+  public void connect() throws ConnectionException {
 
+    try {
+
+      this.client = new QdrantClient(
+          QdrantGrpcClient.newBuilder(host, gprcPort, useTLS)
+              .withApiKey(apiKey)
+              .build()
+      );
+
+      doHealthCheck();
+
+    } catch (ConnectionException e) {
+
+      throw e;
+    } catch (Exception e) {
+
+      throw new ConnectionException("Impossible to connect to Qdrant.", e);
+    }
   }
 
   @Override
   public void disconnect() {
 
+    this.client.close();
   }
 
   @Override
   public boolean isValid() {
-    return false;
+
+    try {
+
+      doHealthCheck();
+      return true;
+
+    } catch (Exception e) {
+
+      LOGGER.error("Failed to validate connection to Qdrant.", e);
+      return false;
+    }
+
+  }
+
+  private void doHealthCheck() throws Exception {
+
+    // Assuming you have a method `healthCheckAsync` that returns ListenableFuture
+    ListenableFuture<QdrantOuterClass.HealthCheckReply> healthCheckFuture = this.client.healthCheckAsync();
+
+    // Make it synchronous by calling get()
+    QdrantOuterClass.HealthCheckReply healthCheckReply = healthCheckFuture.get();
   }
 }
--- src/main/java/org/mule/extension/vectors/internal/connection/store/qdrant/QdrantStoreConnectionProvider.java
+++ src/main/java/org/mule/extension/vectors/internal/connection/store/qdrant/QdrantStoreConnectionProvider.java
@@ -2,6 +2,7 @@ package org.mule.extension.vectors.internal.connection.store.qdrant;
 
 import org.mule.extension.vectors.internal.connection.store.BaseStoreConnection;
 import org.mule.extension.vectors.internal.connection.store.BaseStoreConnectionProvider;
+import org.mule.extension.vectors.internal.connection.store.elasticsearch.ElasticsearchStoreConnection;
 import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.connection.ConnectionValidationResult;
 import org.mule.runtime.extension.api.annotation.Alias;
@@ -23,18 +24,52 @@ public class QdrantStoreConnectionProvider  extends BaseStoreConnectionProvider 
   @Override
   public BaseStoreConnection connect() throws ConnectionException {
 
-    throw new ConnectionException("Failed to connect to Qdrant. Test Connection not supported yet.", null);
+    try {
+
+      QdrantStoreConnection qdrantStoreConnection =
+          new QdrantStoreConnection(qdrantStoreConnectionParameters.getHost(),
+                                    qdrantStoreConnectionParameters.getGprcPort(),
+                                    qdrantStoreConnectionParameters.isUseTLS(),
+                                    qdrantStoreConnectionParameters.getTextSegmentKey(),
+                                    qdrantStoreConnectionParameters.getApiKey());
+      qdrantStoreConnection.connect();
+      return qdrantStoreConnection;
+
+    } catch (ConnectionException e) {
+
+      throw e;
+
+    } catch (Exception e) {
+
+      throw new ConnectionException("Failed to connect to Qdrant", e);
+    }
   }
 
   @Override
   public void disconnect(BaseStoreConnection connection) {
 
+    try {
+
+      connection.disconnect();
+    } catch (Exception e) {
+
+      LOGGER.error("Failed to close connection", e);
+    }
   }
 
   @Override
   public ConnectionValidationResult validate(BaseStoreConnection connection) {
 
-    return ConnectionValidationResult.failure("Failed to validate connection to Qdrant", null);
+    try {
+
+      if (connection.isValid()) {
+        return ConnectionValidationResult.success();
+      } else {
+        return ConnectionValidationResult.failure("Failed to validate connection to Qdrant", null);
+      }
+    } catch (Exception e) {
+      return ConnectionValidationResult.failure("Failed to validate connection to Qdrant", e);
+    }
   }
 
 }
